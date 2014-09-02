88ac824ff413bd4e9bc3f31440c77721b4cffd15
asv88 <alisanmelody@gmail.com>
2014-09-02 12:11:06 -0700

    Shopping List-Revised

--- js/application.js
+++ js/application.js
@@ -1,5 +1,4 @@
 // JavaScript Document
-
 $(document).ready(function () {
 	$(function() {
     $( "#sortable" ).sortable();
@@ -17,23 +16,23 @@ $(document).ready(function () {
 		//gets value of input field
 		var Item = $(".typeItem").val();
 		//listItem variable holds HTML content for appending listItem
-			
+		$(".typeItem").val(" ");
+		//clears input field after submitting item
+		
 		var listItem = $('<li class="listItem">' +
 							'<div class="unchecked"><p></p></div>' + '<div class="checked"><p>&#10003</p></div>'
 							+ '<p>' + Item + '</p>' + '<div class="delete"><p>X</p></div>' +
 							'</li>');
+		if (Item == " ") {
+			  console.log('Not a valid input');
+			  //prevents empty submissions
+		}
+		else {
 		//append listItem <li> to listwrap <ul>					
 		$('.listwrap').append(listItem);
-		//added both states of checked and unchecked then used hide/show() to show unchecked and checked divs
-
-		$(listItem).find(".unchecked").click(function (){
-			$(this).hide();
-			$(this).parent().children(".checked").show();
-		})
-		$(listItem).find(".checked").click(function (){
-			$(this).hide();
-			$(this).parent().children(".unchecked").show();
-		})
+		}
+		
+		
 		//uses mouseenter () and mouseleave () to show delete div
 		$(listItem).mouseenter(function(e) {
        		  $(this).find(".delete").show().click(function (){
@@ -43,6 +42,14 @@ $(document).ready(function () {
 		}).mouseleave(function (e) {
 			 $(this).find(".delete").hide();
 			 //when mouse leaves hide the .delete (X)
+		})
+		//added both states of checked and unchecked then used hide/show() to show unchecked and checked divs
+
+		$(".listwrap").on("click", ".unchecked", function (){
+			$(this).hide().parent().children(".checked").show();
+		})
+		$(".listwrap").on("click", ".checked", function (){
+			$(this).hide().parent().children(".unchecked").show();
 		})
 	});
 });
